a05966969667af21b5a79f4ca4b8ce8dce8a980b
niwaz14 <you@example.com>
2025-07-30 21:27:37 -0400

    New Update: Ajout de DotEnv pour la gestion des variables d'environnement + Implémentation de la connexion à PostgreSQL.

---
+++ Q-LigueAPI/db.js
@@ -0,0 +1,19 @@
+
+require('dotenv').config(); //Rendre les variables d'environnement disponibles.
+
+
+const pg = require('pg'); // On va chercher  le module pg pour interagir avec PostgreSQL.
+const Pool = pg.Pool; // On met en constante la classe Pool de pg, qui nous permet de gérer les connexions à la base de données.
+
+// 3. Create a new instance of the Pool
+// It will use the variables from our .env file to know how to connect.
+const pool = new Pool({ // On initialise une nouvelle instance de Pool.
+  user: process.env.DB_USER, // On load les variables d'environnement pour la connexion à la base de données.
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+}); // Le processus permettra de garder les informations de connexion à la base de données dans un pool de connexions pour sécurisé le processus.
+
+
+module.exports = pool; // On exporte le pool pour pouvoir l'utiliser dans d'autres fichiers de l'application.
--- Q-LigueAPI/index.js
+++ Q-LigueAPI/index.js
@@ -1,7 +1,14 @@
 
+// Installation de DotEnv pour aider à gérer les variables d'environnement
+
+require('dotenv').config(); //Rendre les variables d'environnement disponibles.
+
+
 const express = require('express');
+const pool = require('./db.js'); //Connexion à la base de données PostgreSQL
 const app = express(); // Importer le module express et créer une instance de l'application
 const port = 3000; // Définir le port sur lequel le serveur écoutera
+
 
 
 app.get('/', function(req, res) {
@@ -9,6 +16,24 @@ app.get('/', function(req, res) {
 });
 
 
+
+// On fait un point d'entrée pour récupérer les équipes
+app.get('/api/team', async function(req, res) {
+  try {
+    // On importe la connection à la base de données et on ajoute la constante avec toutes les équipes.
+    const allTeams = await pool.query('SELECT * FROM Team');
+
+    // Si la requête réussit, on envoie les données des équipes en réponse
+    res.json(allTeams.rows);
+
+  } catch (err) {
+    // Si il y a une erreur, on affiche l'erreur dans la console et on envoie un message d'erreur. (500 = Internal Server Error)
+    console.error(err.message);
+    res.status(500).send('Erreur du serveur lors de la récupération des équipes');
+  }
+});
+
+
 app.listen(port, function() {
-  console.log('Le serveur écoute ici : http://localhost:${port}'); // Démarrer le serveur et afficher un message dans la console
+  console.log(`Le serveur écoute ici : http://localhost:${port}`); // Démarrer le serveur et afficher un message dans la console
 });

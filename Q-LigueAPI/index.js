387cf5fc22da387af13c0d3a48460d1fcabf8b8f
niwaz14 <you@example.com>
2025-07-30 22:29:14 -0400

    Ajout des GET/POST API Endpoints

--- Q-LigueAPI/index.js
+++ Q-LigueAPI/index.js
@@ -7,9 +7,10 @@ require('dotenv').config(); //Rendre les variables d'environnement disponibles.
 const express = require('express');
 const pool = require('./db.js'); //Connexion à la base de données PostgreSQL
 const app = express(); // Importer le module express et créer une instance de l'application
+app.use(express.json()); // Middleware pour analyser les requêtes JSON
 const port = 3000; // Définir le port sur lequel le serveur écoutera
 
-
+// |------------------------------------------------------------------------ GET API Endpoint ------------------------------------------------------------------------|
 
 app.get('/', function(req, res) {
   res.send('Salut, Q-Ligue Manager! Le serveur fonctionne!'); // Définir une route pour la racine qui envoie une réponse simple
@@ -33,6 +34,44 @@ app.get('/api/team', async function(req, res) {
   }
 });
 
+// ... On fait un point d'entrée pour récupérer les joueurs
+app.get('/api/players', async function(req, res) {
+  try {
+    
+    const allPlayers = await pool.query('SELECT * FROM Player');
+    res.json(allPlayers.rows);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Erreur du serveur lors de la récupération des joueurs');
+  }
+});
+
+// |------------------------------------------------------------------------  GET ENDPOINTS API FIN ------------------------------------------------------------------------|
+
+// |------------------------------------------------------------------------ POST ENDPOINTS API ------------------------------------------------------------------------|
+
+app.post('/api/games', async function(req, res) {
+  try {
+    
+    const { playerid, matchupid, laneid, gamenumber, gamescore } = req.body;
+
+    // On crée une nouvelle partie dans la base de données avec les paramètres fournis
+    const newGame = await pool.query(
+      "INSERT INTO Game (PlayerID, MatchupID, LaneID, GameNumber, GameScore, GameApprovalStatus) VALUES ($1, $2, $3, $4, $5, 'approved') RETURNING *",
+      [playerid, matchupid, laneid, gamenumber, gamescore]
+    );
+
+    // On l'envoie en réponse
+    res.json(newGame.rows[0]);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erreur du serveur lors de l'envoi de la partie");
+  }
+});
+
+// |------------------------------------------------------------------------ POST ENDPOINTS API FIN ------------------------------------------------------------------------|
 
 app.listen(port, function() {
   console.log(`Le serveur écoute ici : http://localhost:${port}`); // Démarrer le serveur et afficher un message dans la console

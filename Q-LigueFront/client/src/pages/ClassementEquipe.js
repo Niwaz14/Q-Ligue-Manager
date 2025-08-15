fc9d7f619aab7eb5584a42b23238b668d774db9d
niwaz14 <you@example.com>
2025-08-15 01:50:07 -0400

    3.0.1 - Remise à niveau

---
+++ Q-LigueFront/client/src/pages/ClassementEquipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ClassementEquipe.module.css';
+
+function ClassementEquipe() {
+  return (
+    <div className={styles.container}>
+      <h2>Classement des Équipes</h2>
+      <p>Cette page affichera le classement des équipes.</p>
+    </div>
+  );
+}
+
+export default ClassementEquipe;

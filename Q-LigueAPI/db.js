f706f89b2afac2e103b4539fd263c4b3092bd20b
niwaz14 <you@example.com>
2025-09-01 16:31:07 -0400

    4.0.5

--- Q-LigueAPI/db.js
+++ Q-LigueAPI/db.js
@@ -1,16 +1,14 @@
-// Q-LigueAPI/db.js
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// This line checks if the app is running in the production environment on Render.
+
 const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString: connectionString,
-    // This is the crucial part: enable SSL for production, disable it for local development.
+
     ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
